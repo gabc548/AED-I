9a282f6f55315e7be54e0fc525ff41f5c38c0c00
gabc548 <gab.c.sre@gmail.com>
2026-04-10 10:26:13 -0300

    Descrições

--- ProgramaçãoDescomplicada/ExercícioLista10Número2.c
+++ ProgramaçãoDescomplicada/ExercícioLista10Número2.c
@@ -1,5 +1,5 @@
 /** Descrição:
-    Faça um programa que leia do usuário o tamanho de um vetor a ser lido e faça a alocação
+    2. Faça um programa que leia do usuário o tamanho de um vetor a ser lido e faça a alocação
     dinâmica de memória. Em seguida, leia do usuário seus valores e imprima o vetor lido.
 */
 
--- ProgramaçãoDescomplicada/ExercícioLista10Número3.c
+++ ProgramaçãoDescomplicada/ExercícioLista10Número3.c
@@ -1,3 +1,9 @@
+/** Descrição:
+    3. Faça um programa que leia do usuário o tamanho de um vetor a ser lido e faça a alocação
+    dinâmica de memória. Em seguida, leia do usuário seus valores e mostre quantos dos
+    números são pares e quantos são ímpares.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
